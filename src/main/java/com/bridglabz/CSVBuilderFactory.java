7bd0771f66eeed2e498eef99e8fe232a8c00d40b
unknown <himanshugharat44@gmail.com>
2020-08-27 04:52:28 -0500

    MODIFIED the spaces and alignment

--- src/main/java/com/bridglabz/CSVBuilderFactory.java
+++ src/main/java/com/bridglabz/CSVBuilderFactory.java
@@ -7,9 +7,5 @@ public class CSVBuilderFactory {
     public static ICSVBuilder createCSVBuilder() {
         return new OpenCSVBuilder();
     }
-
-    public static OpenCSVBuilder getCSVBean() {
-        return new OpenCSVBuilder();
-
-    }
+    
 }
